1d2d23e144785c609c61adaeb76d57e72a81579c
Minh Nhut <lenhut0929@gmail.com>
2024-08-20 10:35:09 +0700

    add update_by and create_by

--- src/main/java/likelion/edu/vn/health_care/entity/BaseEntity.java
+++ src/main/java/likelion/edu/vn/health_care/entity/BaseEntity.java
@@ -1,9 +1,11 @@
 package likelion.edu.vn.health_care.entity;
 
 import jakarta.persistence.*;
+import likelion.edu.vn.health_care.security.SecurityUtils;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.Optional;
 
 @MappedSuperclass
 @Data
@@ -19,13 +21,23 @@ public abstract class BaseEntity {
     @Column(nullable = true, insertable = false)
     private Date updateTime;
 
+    private String created_by;
+
+    private String updated_by;
+
     @PrePersist
     protected void onCreate() {
+        Optional<String> emailOptional = Optional.ofNullable(SecurityUtils.getEmail());
+        this.created_by = emailOptional.orElse("");
+
         this.createTime = new Date();
     }
 
     @PreUpdate
     protected void onUpdate() {
+        Optional<String> emailOptional = Optional.ofNullable(SecurityUtils.getEmail());
+        this.updated_by = emailOptional.orElse("");
+        
         this.updateTime = new Date();
     }
 }
