68a301f07c042e572d967b4d4b18f887adfef17b
NDHung228 <90383405+NDHung228@users.noreply.github.com>
2024-09-09 09:48:22 +0700

    use log4j (use lib)

--- src/main/java/likelion/edu/vn/health_care/controller/UserController.java
+++ src/main/java/likelion/edu/vn/health_care/controller/UserController.java
@@ -10,6 +10,10 @@ import likelion.edu.vn.health_care.security.UserInfoService;
 import likelion.edu.vn.health_care.service.FileUploadService;
 import likelion.edu.vn.health_care.service.UserService;
 import likelion.edu.vn.health_care.util.ResponseHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,12 +24,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("api/user")
 @CrossOrigin
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -37,7 +42,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    
 
     @GetMapping("/todo/{todoId}")
     public String getTodo(@PathVariable(name = "todoId") Integer todoId) {
@@ -76,6 +80,7 @@ public class UserController {
     @GetMapping("/login")
     public ResponseEntity<Object> login(@RequestParam String email, @RequestParam String password) {
         try {
+            log.info("Log login");
 
             String token = userInfoService.authenticateUser(email, password);
 
